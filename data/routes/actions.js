b4cd987dc0161ee228a95dc826694f99ccaa15f0
M.Real <mreal126@protonmail.com>
2018-08-03 12:34:43 -0400

    Setup is clunky and bad, working on fixing. GETS done if they work, moving to Posts.

--- data/routes/actions.js
+++ data/routes/actions.js
@@ -2,8 +2,31 @@ const actions = require('../helpers/actionModel');
 const express = require('express');
 const router = express.Router();
 
-//GET
+//Maybe add error handling const
 
+//GET
+router.get('/', (req, res) => {
+    actions.get()
+        .then(response => {
+            res.status(200).json(response);
+    })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to get action' });
+    })
+})
+
+router.get('/:id', (req, res) => {
+    actions.get(req.params.id)
+        .then(response => {
+          if(!response) {
+            res.status(404).json({ msg: 'Action with specified id not found' });
+       }
+            res.status(200).json(response);
+    })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to get action'});
+    })
+})
 //POST
 
 //DELETE
--- data/routes/project.js
+++ data/routes/project.js
@@ -3,6 +3,41 @@ const express = require('express');
 const router = express.Router();
 
 //GET
+router.get('/', (req, res) => {
+    project.get()
+        .then(response => {
+            res.status(200).json(response);
+    })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to get project data' });
+    })
+})
+
+router.get('/:id', (req, res) => {
+    project.get(req.params.id)
+        .then(response => {
+          if(!response) {
+            res.status(404).json({ msg: 'Project with specified ID not found' });
+       }
+            res.status(200).json(response);
+    })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to get project data'});
+    })
+})
+
+router.get('/:id/actions', (req, res) => {
+    project.getProjectActions(req.params.id)
+        .then(response => {
+          if(!response) {
+            res.status(404).json({ msg: "Project with specified ID not found" });
+        }     
+            res.status(200).json(response);
+    })
+        .catch(err => {
+            res.status(500).json({ err: "Failed to retrieve project actions" });
+    })
+})
 
 //POST
 

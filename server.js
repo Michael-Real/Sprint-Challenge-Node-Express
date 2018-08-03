300d1dde1ca150d85a3fc5d5202b4c324ebae27c
M.Real <mreal126@protonmail.com>
2018-08-03 12:37:14 -0400

    fixed server.js so it actually links up.

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const actionRoute = require('./data/routes/actionRoute');
-const projectRoute = require('./data/routes/projectRoute');
+const actionRoute = require('./data/routes/actions');
+const projectRoute = require('./data/routes/project');
 
 const server = express();
 
@@ -11,7 +11,7 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
-server.use('/actions, actionRoute');
-server.use('/projects, projectRoute')
+server.use('/actions, actions');
+server.use('/projects, project')
 
 server.listen(8000, () => console.log('API is running on port 8000'));
